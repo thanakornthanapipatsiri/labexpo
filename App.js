5e4bd01125ba924655f7ede77980a62c4dcae76d
SANPONG EIANLHANG <sanpong94@gmail.com>
2022-08-18 00:41:26 +0700

    test01

--- App.js
+++ App.js
@@ -3,22 +3,20 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
- const doIt = () => {
- console.log("Hello from console")
- }
- return (
- <View style={styles.container}>
- <Text onPress={doIt}>Hello world</Text>
- <StatusBar style="auto" />
- </View>
+
+  return (
+    <View style={styles.container}>
+      <Weather zipCode="90110"/>
+      <StatusBar style="auto" />
+    </View>
  );
 }
 
 const styles = StyleSheet.create({
  container: {
- flex: 1,
- backgroundColor: '#fff',
- alignItems: 'center',
- justifyContent: 'center',
+  flex: 1,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  justifyContent: 'center',
  },
 });
