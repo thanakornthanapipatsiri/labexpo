ec94e75eb0c2f1fd58a2de828cb5262d95370b3a
SANPONG EIANLHANG <sanpong94@gmail.com>
2022-08-18 16:20:33 +0700

    part5

--- components/Weather.js
+++ components/Weather.js
@@ -13,15 +13,18 @@ export default function Weather(props) {
 
     return (
         <ImageBackground source={require('../sky.jpg')} style={styles.backdrop}>
+            <Text>Zipcode</Text>
+            <Text>{props.zipCode}</Text>
             <Forecast {...forecastInfo} />
         </ImageBackground>
     )
 }
 
 const styles = StyleSheet.create({
-    backdrop: {
-    alignItems: 'center',
-    width: '100%',
-    height: '100%'
-    },
-   });
+    backdrop : {
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%'
+    }
+})
